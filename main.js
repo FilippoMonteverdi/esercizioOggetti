a9b120e8f58470bfc741b652d1f0f1ad0f374f40
FilippoMonteverdi <145322903+FilippoMonteverdi@users.noreply.github.com>
2025-03-27 10:47:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
-var oggetti = [];
-var n = 0;
-url = "tabella.html";
 
-function add(){
-    var nome = document.getElementById("nome").value;
-    var cognome = document.getElementById("cognome").value;
-    var scuola = document.getElementById("scuola").value;
-    oggetti[n] = {nome:nome, cognome:cognome, scuola:scuola};
-    n++;
-}
-
-function change(){
-    localStorage.setItem("dati", JSON.stringify(oggetti));
-    location.href = url;
-}
