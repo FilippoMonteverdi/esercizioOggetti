1403891485c96c32b0b8be847bc75f89d1f58d20
FilippoMonteverdi <145322903+FilippoMonteverdi@users.noreply.github.com>
2025-03-27 10:48:31 +0100

    Update tabella.js

--- tabella.js
+++ tabella.js
@@ -1,17 +1,1 @@
-function creapagina(){
-    var dati = JSON.parse(localStorage.getItem("dati"));
-    for (var i = 0; i < oggetti.length; i++){
-        var tabella = document.getElementById("tabella");
-        var riga = document.createElement("tr");
-        var el1 = document.createElement("th");
-        el1.innerHTML=dati[i].nome;
-        var el2 = document.createElement("th");
-        el2.innerHTML=dati[i].cognome;
-        var el3 = document.createElement("th");
-        el3.innerHTML=dati[i].scuola;
-        riga.appendChild(el1);
-        riga.appendChild(el2);
-        riga.appendChild(el3);
-        tabella.appendChild(riga);
-    }
-}
+
